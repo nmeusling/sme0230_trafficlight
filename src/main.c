05f98735e8b4ba553c6c5aef46db802697e5a104
Natalie Menato <nmeusling@gmail.com>
2017-04-21 12:14:58 -0300

    Created initial files

---
+++ src/main.c
@@ -0,0 +1,14 @@
+//
+// Created by nmeusling on 4/21/17.
+//
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    scanf("%d", &num);
+    printf("Hello, World!\n");
+    printf("%d", num);
+    return 0;
+}
